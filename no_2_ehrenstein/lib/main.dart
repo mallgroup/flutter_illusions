1ec2c954019e7c30af575abab0d9ec3e78c15b24
Daniel Rataj <daniel.rataj@mall.cz>
2020-06-01 15:10:27 +0200

    Ehrenstein, no. 2

--- no_2_ehrenstein/lib/main.dart
+++ no_2_ehrenstein/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(MyApp());
 
@@ -27,102 +28,39 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    Container square = Container(
+    int lineHeight = 150;
+
+    Widget line = Container(
+      height: (lineHeight * 2).toDouble(),
+      width: 5,
+      color: Colors.black,
+    );
+
+    Widget circle = Container(
       width: 60,
       height: 60,
-      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-    );
-
-    Container circleCover = Container(
-      width: 20,
-      height: 20,
-      color: Colors.white
-    );
-
-    Container squareCover = Container(
-      width: 10,
-      height: 10,
-      color: Colors.white,
-    );
-
-    Widget largeSquare = Container(
-      width: 119,
-      height: 119,
-      child: Stack(
-        children: <Widget>[
-          Positioned(top: 0, left: 0, child: square),
-          Positioned(top: 0, right: 0, child: square),
-          Positioned(bottom: 0, left: 0, child: square),
-          Positioned(bottom: 0, right: 0, child: square),
-          Positioned(top: 0, left: 0, child: squareCover),
-          Positioned(top: 0, right: 0, child: squareCover),
-          Positioned(bottom: 0, left: 0, child: squareCover),
-          Positioned(bottom: 0, right: 0, child: squareCover),
-          Positioned(bottom: 0, left: 0, child: squareCover),
-          Positioned(left: 50, top: 50, child: circleCover),
-        ],
-      ),
+      decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white),
     );
 
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Stack(
-        children: <Widget>[
-          Positioned.fill(
-              left: (MediaQuery.of(context).size.width / 2) - (119 * 3 / 2),
-              top: (MediaQuery.of(context).size.height / 2) - (119 * 2),
-              child: Stack(
-                children: <Widget>[
-                  Positioned(
-                    top: 0,
-                    left: 0,
-                    child: largeSquare,
-                  ),
-                  Positioned(
-                    top: 0,
-                    left: 118, // -1 = padding
-                    child: largeSquare,
-                  ),
-                  Positioned(
-                    top: 0,
-                    left: 236, // +2 = padding
-                    child: largeSquare,
-                  ),
-                  Positioned(
-                    top: 118,
-                    left: 0, // +2 = padding
-                    child: largeSquare,
-                  ),
-                  Positioned(
-                    top: 118,
-                    left: 118,
-                    child: largeSquare,
-                  ),
-                  Positioned(
-                    top: 118,
-                    left: 236,
-                    child: largeSquare,
-                  ),
-                  Positioned(
-                    top: 236,
-                    left: 0, // +2 = padding
-                    child: largeSquare,
-                  ),
-                  Positioned(
-                    top: 236,
-                    left: 118,
-                    child: largeSquare,
-                  ),
-                  Positioned(
-                    top: 236,
-                    left: 236,
-                    child: largeSquare,
-                  ),
-                ],
-              ))
-        ],
+      body: Center(
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: <Widget>[
+            line,
+            Transform.rotate(angle: pi, child: line),
+            Transform.rotate(angle: pi / 2, child: line),
+            Transform.rotate(angle: pi / 3, child: line),
+            Transform.rotate(angle: pi / 6, child: line),
+            Transform.rotate(angle: pi / -3, child: line),
+            Transform.rotate(angle: pi / -6, child: line),
+            circle
+          ],
+        ),
       ),
     );
   }
