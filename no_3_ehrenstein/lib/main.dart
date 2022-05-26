90e8356ebbb93ea5516457559e5821f47df5b53a
Daniel Rataj <daniel.rataj@centrum.cz>
2022-05-26 16:12:48 +0200

    No 3 ehrenstein (#2)
    * Ehrenstein, no. 3

---
+++ no_3_ehrenstein/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+const String title = 'Ehrenstein, no. 3';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: title),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Widget circle() {
+    return Container(
+      height: 150,
+      width: 150,
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          color: Colors.blue, borderRadius: BorderRadius.circular(100)),
+    );
+  }
+
+  Widget square() {
+    return Container(
+      width: 170,
+      height: 170,
+      color: Colors.white,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [circle(), circle()],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [circle(), circle()],
+                ),
+              ],
+            ),
+            square()
+          ]),
+        ));
+  }
+}
